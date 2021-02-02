e93cc73b01784d94e91143e86a4eed814f4aa6ca
FletchyAF <joshua.f1998@gmail.com>
2021-02-02 16:13:22 -0500

    commit tree e961f51c2dfed970334eb0d4aebd140cf5430e39

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
-  # write code here
+  
+  attr_accessor :length_one, :length_two, :length_three
+
+  def initialize(length_one, length_two, length_three)
+    @length_one = length_one
+    @length_two = length_two
+    @length_three = length_three
+  end
+
+  def kind
+    sides = [@length_one, @length_two, @length_three].uniq
+    type = case sides.length
+      when 1 then :equilateral
+      when 2 then :isosceles
+      when 3 then :scalene
+      when 0 
+        begin
+        raise TriangleError
+        puts error.message
+        end
+    end
+
+  end
+
+  class TriangleError < StandardError
+    def message
+    end
+  end
+
 end
